67116bf4063b1aa1667959e52e11808dadd20ec9
Yuriy Sydor <yuriysydor1991@gmail.com>
2023-11-12 21:08:25 +0200

    translations: added de_DE locale and reduced script-vars
    With automated translation

---
+++ resources/Qt1SimpleTextEditorDemo_de_DE.ts
@@ -0,0 +1,233 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AboutDialogEmbellisher</name>
+    <message>
+        <source>About window</source>
+        <translation>Über Fenster</translation>
+    </message>
+    <message>
+        <source>Visit project github repository </source>
+        <translation>Besuchen Sie das Github-Repository des Projekts </translation>
+    </message>
+    <message>
+        <source> for updates and additional info</source>
+        <translation> für Updates und zusätzliche Informationen</translation>
+    </message>
+    <message>
+        <source>Created by </source>
+        <translation>Erstellt von </translation>
+    </message>
+    <message>
+        <source>Visit home page for more </source>
+        <translation>Weitere Informationen finden Sie auf der Homepage </translation>
+    </message>
+    <message>
+        <source>Build date: </source>
+        <translation>Baudatum: </translation>
+    </message>
+    <message>
+        <source> and git commit: </source>
+        <translation> und git commit: </translation>
+    </message>
+    <message>
+        <source>About used Qt5 framework</source>
+        <translation>Über das verwendete Qt5-Framework</translation>
+    </message>
+</context>
+<context>
+    <name>FileDealer</name>
+    <message>
+        <source>Error while creating reader for a file: </source>
+        <translation>Fehler beim Erstellen des Readers für eine Datei: </translation>
+    </message>
+    <message>
+        <source>Failure to read the file: </source>
+        <translation>Fehler beim Lesen der Datei: </translation>
+    </message>
+    <message>
+        <source>Open a text file</source>
+        <translation>Öffnen Sie eine Textdatei</translation>
+    </message>
+    <message>
+        <source>No file path given</source>
+        <translation>Kein Dateipfad angegeben</translation>
+    </message>
+    <message>
+        <source>Failure to save the file: </source>
+        <translation>Fehler beim Speichern der Datei: </translation>
+    </message>
+    <message>
+        <source>Save File As</source>
+        <translation>Datei speichern als</translation>
+    </message>
+    <message>
+        <source>Failure to open file: </source>
+        <translation>Fehler beim Öffnen der Datei: </translation>
+    </message>
+    <message>
+        <source>Failure to save file as: </source>
+        <translation>Fehler beim Speichern der Datei als: </translation>
+    </message>
+    <message>
+        <source>&lt;No file opened&gt;</source>
+        <translation>&lt;Keine Datei geöffnet&gt;</translation>
+    </message>
+    <message>
+        <source>What to do with the unsaved data?</source>
+        <translation>Was tun mit den nicht gespeicherten Daten?</translation>
+    </message>
+</context>
+<context>
+    <name>FindEmbellisher</name>
+    <message>
+        <source>Find text previous</source>
+        <translation>Suchen Sie den vorherigen Text</translation>
+    </message>
+    <message>
+        <source>Find text next</source>
+        <translation>Suchen Sie als nächstes den Text</translation>
+    </message>
+    <message>
+        <source>Input the search text:</source>
+        <translation>Geben Sie den Suchtext ein:</translation>
+    </message>
+</context>
+<context>
+    <name>GoToLineEmbellisher</name>
+    <message>
+        <source>Go to line</source>
+        <translation>Gehen Sie zur Zeile</translation>
+    </message>
+    <message>
+        <source>Line </source>
+        <translation>Linie </translation>
+    </message>
+</context>
+<context>
+    <name>HtmlDealer</name>
+    <message>
+        <source>Wrap &lt;b&gt;bold&lt;/b&gt;</source>
+        <translation>Umbruch &lt;b&gt;fett&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;i&gt;italic&lt;/i&gt;</source>
+        <translation>Zeilenumbruch &lt;i&gt;kursiv&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;u&gt;underline&lt;/u&gt;</source>
+        <translation>Umbrechen Sie &lt;u&gt;Unterstreichung&lt;/u&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;a href=&quot;&quot;...&gt;</source>
+        <translation>Wrap &lt;a href=&quot;&quot;...&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h1&gt;</source>
+        <translation>Wickeln Sie &lt;h1&gt; ein</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h2&gt;</source>
+        <translation>Wickeln Sie &lt;h2&gt; ein</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h3&gt;</source>
+        <translation>Wickeln Sie &lt;h3&gt; ein</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h4&gt;</source>
+        <translation>Wickeln Sie &lt;h4&gt; ein</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h5&gt;</source>
+        <translation>Wickeln Sie &lt;h5&gt; ein</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h6&gt;</source>
+        <translation>Wickeln Sie &lt;h6&gt; ein</translation>
+    </message>
+</context>
+<context>
+    <name>MenuEmbellisher</name>
+    <message>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <source>Find</source>
+        <translation>Finden</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Helfen</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Neu</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Offen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Datei speichern</translation>
+    </message>
+    <message>
+        <source>SaveAs</source>
+        <translation>Datei speichern als</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopieren</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Schneiden</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Daten einfügen</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Rückgängig machen</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Wiederholen</translation>
+    </message>
+    <message>
+        <source>Find backwards</source>
+        <translation>Rückwärts finden</translation>
+    </message>
+    <message>
+        <source>Go to line</source>
+        <translation>Gehen Sie zur Zeile</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über das Programm</translation>
+    </message>
+    <message>
+        <source>About Qt5</source>
+        <translation>Über das Qt5-Programm</translation>
+    </message>
+</context>
+<context>
+    <name>TxtDealer</name>
+    <message>
+        <source>Error while opening the file: </source>
+        <translation>Fehler beim Öffnen der Datei: </translation>
+    </message>
+</context>
+</TS>

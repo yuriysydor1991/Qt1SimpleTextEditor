43b2aee8611013556549ee5b45907e16b6b0cc53
Yuriy Sydor <yuriysydor1991@gmail.com>
2023-11-12 20:50:14 +0200

    translations: added initial pl_PL machine locale

---
+++ resources/Qt1SimpleTextEditorDemo_pl_PL.ts
@@ -0,0 +1,233 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AboutDialogEmbellisher</name>
+    <message>
+        <source>About window</source>
+        <translation>O oknie</translation>
+    </message>
+    <message>
+        <source>Visit project github repository </source>
+        <translation>Odwiedź repozytorium projektu na Githubie </translation>
+    </message>
+    <message>
+        <source> for updates and additional info</source>
+        <translation> w celu uzyskania aktualizacji i dodatkowych informacji</translation>
+    </message>
+    <message>
+        <source>Created by </source>
+        <translation>Stworzone przez </translation>
+    </message>
+    <message>
+        <source>Visit home page for more </source>
+        <translation>Odwiedź stronę główną, aby uzyskać więcej </translation>
+    </message>
+    <message>
+        <source>Build date: </source>
+        <translation>Data budowy: </translation>
+    </message>
+    <message>
+        <source> and git commit: </source>
+        <translation> i git zatwierdzić: </translation>
+    </message>
+    <message>
+        <source>About used Qt5 framework</source>
+        <translation>O używanym frameworku Qt5</translation>
+    </message>
+</context>
+<context>
+    <name>FileDealer</name>
+    <message>
+        <source>Error while creating reader for a file: </source>
+        <translation>Błąd podczas tworzenia czytnika dla pliku: </translation>
+    </message>
+    <message>
+        <source>Failure to read the file: </source>
+        <translation>Nie udało się odczytać pliku: </translation>
+    </message>
+    <message>
+        <source>Open a text file</source>
+        <translation>Otwórz plik tekstowy</translation>
+    </message>
+    <message>
+        <source>No file path given</source>
+        <translation>Nie podano ścieżki pliku</translation>
+    </message>
+    <message>
+        <source>Failure to save the file: </source>
+        <translation>Nie udało się zapisać pliku:</translation>
+    </message>
+    <message>
+        <source>Save File As</source>
+        <translation>Zapisz plik jako</translation>
+    </message>
+    <message>
+        <source>Failure to open file: </source>
+        <translation>Nie udało się otworzyć pliku: </translation>
+    </message>
+    <message>
+        <source>Failure to save file as: </source>
+        <translation>Nie udało się zapisać pliku jako: </translation>
+    </message>
+    <message>
+        <source>&lt;No file opened&gt;</source>
+        <translation>&lt;Nie otwarto żadnego pliku&gt;</translation>
+    </message>
+    <message>
+        <source>What to do with the unsaved data?</source>
+        <translation>Co zrobić z niezapisanymi danymi?</translation>
+    </message>
+</context>
+<context>
+    <name>FindEmbellisher</name>
+    <message>
+        <source>Find text previous</source>
+        <translation>Znajdź poprzedni tekst</translation>
+    </message>
+    <message>
+        <source>Find text next</source>
+        <translation>Znajdź następny tekst</translation>
+    </message>
+    <message>
+        <source>Input the search text:</source>
+        <translation>Wpisz wyszukiwany tekst:</translation>
+    </message>
+</context>
+<context>
+    <name>GoToLineEmbellisher</name>
+    <message>
+        <source>Go to line</source>
+        <translation>Przejdź do linii</translation>
+    </message>
+    <message>
+        <source>Line </source>
+        <translation>Linia</translation>
+    </message>
+</context>
+<context>
+    <name>HtmlDealer</name>
+    <message>
+        <source>Wrap &lt;b&gt;bold&lt;/b&gt;</source>
+        <translation>Zawijaj &lt;b&gt;pogrubieniem&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;i&gt;italic&lt;/i&gt;</source>
+        <translation>Zawijaj &lt;i&gt;kursywą&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;u&gt;underline&lt;/u&gt;</source>
+        <translation>Zawiń &lt;u&gt;podkreśl&lt;/u&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;a href=&quot;&quot;...&gt;</source>
+        <translation>Zawiń &lt;a href=&quot;&quot;...&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h1&gt;</source>
+        <translation>Zawiń &lt;h1&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h2&gt;</source>
+        <translation>Zawiń &lt;h2&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h3&gt;</source>
+        <translation>Zawiń &lt;h3&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h4&gt;</source>
+        <translation>Zawiń &lt;h4&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h5&gt;</source>
+        <translation>Zawiń &lt;h5&gt;</translation>
+    </message>
+    <message>
+        <source>Wrap &lt;h6&gt;</source>
+        <translation>Zawiń &lt;h6&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MenuEmbellisher</name>
+    <message>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <source>Find</source>
+        <translation>Znajdować</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Edytować</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Nowy</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Otwarty</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Zapisz plik</translation>
+    </message>
+    <message>
+        <source>SaveAs</source>
+        <translation>Zapisz jako</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Zamknąć</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiuj</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Cięcie</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Wklej</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Cofnij</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Wykonaj ponownie</translation>
+    </message>
+    <message>
+        <source>Find backwards</source>
+        <translation>Znajdź od tyłu</translation>
+    </message>
+    <message>
+        <source>Go to line</source>
+        <translation>Przejdź do linii</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <source>About Qt5</source>
+        <translation>O programie Qt5</translation>
+    </message>
+</context>
+<context>
+    <name>TxtDealer</name>
+    <message>
+        <source>Error while opening the file: </source>
+        <translation>Błąd podczas otwierania pliku: </translation>
+    </message>
+</context>
+</TS>

7728e6053232ab644c5cbae55a798a9814955934
Yuriy Sydor <yuriysydor1991@gmail.com>
2023-11-14 12:56:14 +0200

    FilterCreator: inserted comments for a Doxygen documentation

--- src/window/file-dealers/FilterCreator.h
+++ src/window/file-dealers/FilterCreator.h
@@ -5,10 +5,19 @@
 #ifndef QT1SIMPLETEXTEDITORDEMO_FILTERCREATOR_H
 #define QT1SIMPLETEXTEDITORDEMO_FILTERCREATOR_H
 
+/**
+ * Factory class which encapsulates code for a
+ * open Qt dialog file filter subsystem.
+ */
 class FilterCreator {
  public:
   FilterCreator() = default;
 
+  /**
+   * Creates and returns pointer to a string which contains
+   * filters for a file filter facility of an open dialog.
+   * @return Created file filter pointer to a char string.
+   */
   static const char* defaultTxtFilter();
 };
 

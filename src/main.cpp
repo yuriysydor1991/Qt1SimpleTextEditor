7f1f83582c73fb18f660cb77fae59af29393fadf
Yuriy Sydor <yuriysydor1991@gmail.com>
2023-11-14 23:07:49 +0200

    main.cpp: inserted comments for a Doxygen documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
 #include "window/WindowFactory.h"
 
+/**
+ * The main application binary function, obviously.
+ * @param argc Number of the arguments given by the
+ * system for a current application run which are stored
+ * in the argv argument.
+ * @param argv All the arguments given to the application
+ * binary by the system.
+ * @return returns the application exit code. Particularly in this
+ * case it's QApplication's exec() method return value call of
+ * which is encapsulated in the WindowFactory methods.
+ */
 int main(int argc, char** argv) { return WindowFactory::execute(argc, argv); }
